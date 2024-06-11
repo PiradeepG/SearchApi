e95be48c5e54c50b85d749d6c1605fdacb8496d5
Piradeep <piradeep@pirad-zstch1299.csez.zohocorpin.com>
2024-06-11 16:37:49 +0530

    completed paranthesis checking

--- src/error.rs
+++ src/error.rs
@@ -4,4 +4,5 @@ pub enum Error{
     ReadLine,
     JsonParsing,
     GettingOutput,
+    InvalidQuery,
 }
--- src/functions.rs
+++ src/functions.rs
@@ -12,8 +12,13 @@ pub fn get_keys_and_query(query: String) -> (Vec<String>, Vec<String>) {
         return  (query_parts, key_vec)
     }
 
-    for i in (0..query_parts.len()).step_by(2){
-        let temp_check=query_parts[i].split(':').collect::<Vec<&str>>()[0].to_string();
+    for i in &query_parts{
+
+        if i=="and" || i=="or" || i== "(" || i==")"{
+            continue;
+        } 
+
+        let temp_check=i.split(':').collect::<Vec<&str>>()[0].to_string();
         if !key_vec.contains(&temp_check){
             key_vec.push(temp_check);
         }
@@ -56,7 +61,9 @@ pub fn get_matched_lines(file_path:String,keys:&Vec<String>,query_vec:&Vec<Strin
             Err(_) => return Err(Error::JsonParsing),
         };
         let (result,last)=complete_check(query_vec,&temp_str,&mut 0);
-        println!("{last}");
+        if last != query_vec.len()-1{
+            return Err(Error::InvalidQuery);
+        }
         if result{
             res_vec.push(json_obj);
         }
@@ -64,43 +71,9 @@ pub fn get_matched_lines(file_path:String,keys:&Vec<String>,query_vec:&Vec<Strin
     Ok(res_vec)
 }
 
-
-pub fn is_matching(query_vec:&Vec<String>,line:String)->bool{
-    let mut temp=true;
-    let mut i=0;
-
-    while i < query_vec.len(){
-        let temp_check=&query_vec[i];
-        match temp_check.as_str(){
-            "and" => {
-                if temp {
-                    i += 1;
-                } else {
-                    i += 2;
-                }
-                continue;
-            },
-            "or" => {
-                if temp {
-                    i += 2;
-                } else {
-                    i += 1;
-                }
-                continue;
-            },
-            _ => {
-                temp = line.contains(temp_check);
-                i+=1;
-            }
-        }
-    }
-    temp
-}
 fn complete_check(query_vec:&Vec<String>,line:&String,index:&mut usize)->(bool,usize){
-
     let mut temp=true;
     while *index < query_vec.len(){
-        println!("{index}");
         let temp_check=&query_vec[*index];
 
         match temp_check.as_str() { 
@@ -183,5 +156,3 @@ fn complete_check(query_vec:&Vec<String>,line:&String,index:&mut usize)->(bool,u
     }
     (temp,*index)
 }
-
-
